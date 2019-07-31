e5df550c10d6889b6fc3d7844188df02e08d8654
piyush <piyushr999@gmail.com>
2019-07-31 14:11:39 +0530

    IMpl changes

--- src/main/java/com/hcl/product/service/ReadDataServiceImpl.java
+++ src/main/java/com/hcl/product/service/ReadDataServiceImpl.java
@@ -3,7 +3,9 @@ package com.hcl.product.service;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -27,6 +29,7 @@ public class ReadDataServiceImpl implements ReadDataService {
 			// Iterate through each rows one by one
 			Iterator<Row> rowIterator = sheet.iterator();
 			
+			List<Product> products = new ArrayList<Product>();			
 			while (rowIterator.hasNext()) {
 				Product product = new Product();
 				Row row = rowIterator.next();
@@ -35,25 +38,8 @@ public class ReadDataServiceImpl implements ReadDataService {
 				product.setProductName(row.getCell(1).getStringCellValue());
 				product.setQuantity(Integer.parseInt(row.getCell(2).getStringCellValue()));
 				product.setManufacturedBy(row.getCell(3).getStringCellValue());
-				
-				
-				// For each row, iterate through all the columns
-				Iterator<Cell> cellIterator = row.cellIterator();
-
-				while (cellIterator.hasNext()) {
-			
-					Cell cell = cellIterator.next();
-					// Check the cell type and format accordingly
-					switch (cell.getCellType()) {
-					case Cell.CELL_TYPE_NUMERIC:
-						System.out.print(cell.getNumericCellValue() + "t");
-						break;
-					case Cell.CELL_TYPE_STRING:
-						System.out.print(cell.getStringCellValue() + "t");
-						break;
-					}
-				}
-				System.out.println("");
+						
+				products.add(product);
 			}
 			file.close();
 		} catch (IOException e) {
