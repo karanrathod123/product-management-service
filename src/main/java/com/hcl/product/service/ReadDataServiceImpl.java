f9d1ac3856ca8796b686c14ed731157c5f7d7966
piyush <piyushr999@gmail.com>
2019-07-31 12:12:48 +0530

    adding service classes

---
+++ src/main/java/com/hcl/product/service/ReadDataServiceImpl.java
@@ -0,0 +1,12 @@
+package com.hcl.product.service;
+
+import com.hcl.product.dto.ResponseData;
+
+public class ReadDataServiceImpl implements ReadDataService {
+
+	@Override
+	public ResponseData readData(String filePath) {
+		return null;
+	}
+
+}
