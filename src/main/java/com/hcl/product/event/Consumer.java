0d6e723c062603b563ebe600b876ce0c45629431
piyush <piyushr999@gmail.com>
2019-07-31 17:45:47 +0530

    Fixing code smell

--- src/main/java/com/hcl/product/event/Consumer.java
+++ src/main/java/com/hcl/product/event/Consumer.java
@@ -1,9 +1,9 @@
 package com.hcl.product.event;
 
-import javax.jms.JMSException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
+
 import com.hcl.product.service.ReadDataService;
 
 @Component
@@ -13,7 +13,7 @@ public class Consumer {
 	private ReadDataService readDataServiceImpl;
 
 	@JmsListener(destination = "product.queue", containerFactory = "myFactory")
-	public void receiveMessage(final ProductEvent event) throws JMSException {
+	public void receiveMessage(final ProductEvent event) {
 		readDataServiceImpl.saveDatetoDatabase(event.getProductList());
 	}
 
