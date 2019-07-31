56f648c4274ffe2d796b81fde70de75b4ec06dda
prajaktajoshi123 <joshiprajakta12493@gmail.com>
2019-07-31 17:54:08 +0530

    updated junit for AllProductServiceImplTest

--- src/test/java/com/hcl/product/service/AllProductServiceImplTest.java
+++ src/test/java/com/hcl/product/service/AllProductServiceImplTest.java
@@ -1,9 +1,9 @@
 package com.hcl.product.service;
 
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.Before;
@@ -23,42 +23,48 @@ public class AllProductServiceImplTest {
 
 	@Mock
 	ProductRepository productRepository;
-	
+
 	@InjectMocks
 	AllProductServiceImpl allProductServiceImpl;
-	
-	Product product1=new Product();
-	Product product=new Product();
-	List<Product> productlist=new ArrayList<>();
-	
+
+	Product product1 = new Product();
+	Product product = new Product();
+	List<Product> productlist = new ArrayList<>();
+
 	@Before
-	public void setUp()
-	{
-		
-		
+	public void setUp() {
+
 		product1.setManufacturedBy("hcl");
-        product1.setProductId(101l);	
-        product1.setProductName("laptop");
-        product1.setProductVersion(1.0);
-        product1.setQuantity(20);
-        
-        product.setManufacturedBy("intel");
-        product.setProductId(102l);	
-        product.setProductName("processor");
-        product.setProductVersion(2.0);
-        product.setQuantity(10);
-        
-        productlist.add(product);
-        productlist.add(product1);
-        
+		product1.setProductId(101l);
+		product1.setProductName("laptop");
+		product1.setProductVersion(1.0);
+		product1.setQuantity(20);
+
+		product.setManufacturedBy("intel");
+		product.setProductId(102l);
+		product.setProductName("processor");
+		product.setProductVersion(2.0);
+		product.setQuantity(10);
+
+		productlist.add(product);
+		productlist.add(product1);
+
 	}
-	
+
 	@Test
-	public void getAllProductTest()
-	{
+	public void getAllProductTest() {
+
 		Mockito.when(productRepository.findByProductId(product.getProductId())).thenReturn(productlist);
-		ResponseData response= allProductServiceImpl.getAllProduct(product.getProductId());
+		ResponseData response = allProductServiceImpl.getAllProduct(product.getProductId());
 		assertNotNull(response);
 	}
-	
+
+	@Test
+	public void getAllProductTestWhenEmpty() {
+
+		Mockito.when(productRepository.findByProductId(product.getProductId())).thenReturn(Collections.emptyList());
+		ResponseData response = allProductServiceImpl.getAllProduct(product.getProductId());
+		assertNotNull(response);
+	}
+
 }
